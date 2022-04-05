c1145a5ef62386191f9a7998d8adc7c794b8ce22
Ildar <khabibulin_ildarr@mail.ru>
2022-04-06 02:40:41 +0300

    added TagsGrid, WorkoutListView look is ready

--- src/main/java/com/example/views/workoutlist/WorkoutListView.java
+++ src/main/java/com/example/views/workoutlist/WorkoutListView.java
@@ -2,10 +2,8 @@ package com.example.views.workoutlist;
 
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasStyle;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Main;
-import com.vaadin.flow.component.html.OrderedList;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
@@ -13,7 +11,8 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-import javax.annotation.security.PermitAll;
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("Workout List")
 @Route(value = "workout-list")
@@ -21,23 +20,17 @@ import javax.annotation.security.PermitAll;
 @AnonymousAllowed
 public class WorkoutListView extends Main implements HasComponents, HasStyle {
 
-    private OrderedList imageContainer;
+    private OrderedList workoutContainer;
+    private Label tagsLabel;
+    private static TagsGrid tagsGrid;
 
     public WorkoutListView() {
         constructUI();
 
-        imageContainer.add(new WorkoutListViewCard("Snow mountains under stars",
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new WorkoutListViewCard("Snow covered mountain",
-                "https://images.unsplash.com/photo-1512273222628-4daea6e55abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new WorkoutListViewCard("River between mountains",
-                "https://images.unsplash.com/photo-1536048810607-3dc7f86981cb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"));
-        imageContainer.add(new WorkoutListViewCard("Milky way on mountains",
-                "https://images.unsplash.com/photo-1515705576963-95cad62945b6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new WorkoutListViewCard("Mountain with fog",
-                "https://images.unsplash.com/photo-1513147122760-ad1d5bf68cdb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"));
-        imageContainer.add(new WorkoutListViewCard("Mountain at night",
-                "https://images.unsplash.com/photo-1562832135-14a35d25edef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=815&q=80"));
+        workoutContainer.add(new WorkoutListViewCard("part of body1"));
+        workoutContainer.add(new WorkoutListViewCard("part of body2"));
+        workoutContainer.add(new WorkoutListViewCard("part of body3"));
+
 
     }
 
@@ -48,7 +41,7 @@ public class WorkoutListView extends Main implements HasComponents, HasStyle {
         container.addClassNames("items-center", "justify-between");
 
         VerticalLayout headerContainer = new VerticalLayout();
-        H2 header = new H2("Beautiful photos");
+        H2 header = new H2("Workouts");
         header.addClassNames("mb-0", "mt-xl", "text-3xl");
         Paragraph description = new Paragraph("Royalty free photos and pictures, courtesy of Unsplash");
         description.addClassNames("mb-xl", "mt-0", "text-secondary");
@@ -59,11 +52,14 @@ public class WorkoutListView extends Main implements HasComponents, HasStyle {
         sortBy.setItems("Popularity", "Newest first", "Oldest first");
         sortBy.setValue("Popularity");
 
-        imageContainer = new OrderedList();
-        imageContainer.addClassNames("gap-m", "grid", "list-none", "m-0", "p-0");
+        workoutContainer = new OrderedList();
+        workoutContainer.addClassNames("gap-m", "grid", "list-none", "m-0", "p-0");
 
+        tagsGrid = new TagsGrid();
+        tagsGrid.setWidth("50%");
+        tagsGrid.addClassNames("py-m");
         container.add(header, sortBy);
-        add(container, imageContainer);
+        add(container, tagsGrid, workoutContainer);
 
     }
 }
