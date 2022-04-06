dae59b270c1a4f8aec5286f64f93a2c4be9035be
Ildar <khabibulin_ildarr@mail.ru>
2022-04-07 00:08:01 +0300

    CreateWorkoutView look refactor

--- src/main/java/com/example/views/createworkout/CreateWorkoutView.java
+++ src/main/java/com/example/views/createworkout/CreateWorkoutView.java
@@ -1,69 +1,54 @@
 package com.example.views.createworkout;
 
-
+import com.example.views.tagsgrid.TagsGrid;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-import javax.annotation.security.PermitAll;
-
 @PageTitle("Create Workout")
 @Route(value = "create-workout")
 //@PermitAll
 @AnonymousAllowed
 public class CreateWorkoutView extends VerticalLayout {
-    private final Component header;
-    private TextField name;
-    private TextArea body;
-    private final Button button;
-    private final Label tagsLabel;
-    private final Label bodyLabel;
-    private final Label nameLabel;
-    private final Button exit;
 
-    private static Grid<String> tags;
+    private static final Component HEADER = new H2("Create your workout");
+    private static final Label TAGS_LABEL = new Label("Add tags to your workout");
+    private static final Label BODY_LABEL = new Label("Add your workout's description");
+    private static final Label NAME_LABEL = new Label("Add your workout's name");
+
+    private final TextField name;
+    private final TextArea body;
+    private final Button button;
+
+    private final TagsGrid tagsGrid;
 
     public CreateWorkoutView() {
-        header = new H2("Creating your workout");
-        nameLabel = new Label("Add your workout's name");
         name = new TextField();
 
-        bodyLabel = new Label("Add your workout's description");
         body = new TextArea();
         body.setWidthFull();
 
         button = new Button("Publish");
+        button.addClickListener(e -> System.out.println("Publish workout"));
 
-        tagsLabel = new Label("Add tags to your workout");
-        tags = new Grid<>(String.class, false);
-        tags.setAllRowsVisible(true);
+        tagsGrid = new TagsGrid();
+        tagsGrid.setWidth("50%");
 
-        exit = new Button("Exit");
-        exit.addClickListener(e -> UI.getCurrent().navigate("start-page"));
-        exit.setWidth("10%");
-        exit.setHeight("40px");
-        exit.addThemeVariants(ButtonVariant.LUMO_SMALL);
-
-        setHorizontalComponentAlignment(Alignment.CENTER, header);
-
-        VerticalLayout info = new VerticalLayout(nameLabel, name, tagsLabel, tags, bodyLabel, body, button);
+        VerticalLayout info = new VerticalLayout(NAME_LABEL, name, TAGS_LABEL, tagsGrid, BODY_LABEL, body, button);
         info.setWidth("70%");
-        info.setHorizontalComponentAlignment(Alignment.END, exit);
-        info.setHorizontalComponentAlignment(Alignment.CENTER, button);
-        setHorizontalComponentAlignment(Alignment.CENTER, info);
 
-        add(exit, header, info);
+        setHorizontalComponentAlignment(Alignment.CENTER, HEADER);
+        setHorizontalComponentAlignment(Alignment.CENTER, info);
+        info.setHorizontalComponentAlignment(Alignment.CENTER, button);
+
+        add(HEADER, info);
     }
 
 }
