c65a226b5e3754dab6c0bd9db51cf3cf29f540af
Ildar <khabibulin_ildarr@mail.ru>
2022-04-07 00:15:08 +0300

    WorkoutView look refactor

--- src/main/java/com/example/views/workout/WorkoutView.java
+++ src/main/java/com/example/views/workout/WorkoutView.java
@@ -1,12 +1,9 @@
 package com.example.views.workout;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.details.DetailsVariant;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.ListItem;
 import com.vaadin.flow.component.html.UnorderedList;
@@ -28,16 +25,11 @@ public class WorkoutView extends VerticalLayout {
     private Component header;
     private Label text;
 
-    private final Button exit;
 
     public WorkoutView() {
-        header = new H1("name_of_workout");
-        text = new Label("do hardaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaa"+
-                "sssssssssssssssssssssss sssssssssssssssssssssssssssss ssssssssssssssssssssssssssssss ssssssssssssssssssss"+
-                "rrrrrrrrrrrrrrr rrrrrrrrrrrrrrrrrrrr rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr rrrrrrrrrrrrrrr");
+        header = new H2("name_of_workout");
+        text = new Label("do hard");
         text.setWidth("70%");
-        exit = new Button("Exit");
-        exit.addClickListener(e -> UI.getCurrent().navigate("start-page"));
 
         RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
         radioGroup.setLabel("Please rate the workout");
@@ -48,8 +40,9 @@ public class WorkoutView extends VerticalLayout {
         UnorderedList content = new UnorderedList(
                 new ListItem("бицепс"),
                 new ListItem("глаз")
-
         );
+
+        content.add();
 
         Details details = new Details("Tags", content);
         details.setOpened(true);
@@ -57,17 +50,14 @@ public class WorkoutView extends VerticalLayout {
         details.setWidth("70%");
 
         setSizeFull();
-        exit.setWidth("10%");
-        exit.setHeight("6%");
-        exit.addThemeVariants(ButtonVariant.LUMO_SMALL);
 
-        setHorizontalComponentAlignment(Alignment.END,exit);
+        setHorizontalComponentAlignment(Alignment.END);
         setHorizontalComponentAlignment(Alignment.CENTER, header);
         setHorizontalComponentAlignment(Alignment.CENTER, text);
         setHorizontalComponentAlignment(Alignment.CENTER,rateButtons);
         setHorizontalComponentAlignment(Alignment.CENTER,details);
 
-        add(exit, header,details, text, rateButtons);
+        add(header,details, text, rateButtons);
     }
 
 }
