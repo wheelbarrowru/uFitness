110fc6855e51a316e02a27c7ae7b6e7942600f81
Ildar <khabibulin_ildarr@mail.ru>
2022-04-10 14:07:52 +0300

    added Ufit.db

--- src/main/java/com/example/data/repository/WorkoutRepository.java
+++ src/main/java/com/example/data/repository/WorkoutRepository.java
@@ -2,9 +2,11 @@ package com.example.data.repository;
 
 import com.example.data.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
-public interface WorkoutRepository extends JpaRepository<Workout, UUID> {
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
     //читай статью
+    Workout findWorkoutById(int id);
 }
