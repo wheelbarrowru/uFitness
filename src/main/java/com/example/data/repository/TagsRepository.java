6ab95a42920d31ac4bc12546fdee29475a6b0ead
mvtValeev <75842546+mvtValeev@users.noreply.github.com>
2022-04-10 23:43:13 +0300

    update sql_sr.sql

--- src/main/java/com/example/data/repository/TagsRepository.java
+++ src/main/java/com/example/data/repository/TagsRepository.java
@@ -1,10 +1,14 @@
 package com.example.data.repository;
 
 import com.example.data.model.Tags;
+import com.example.data.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagsRepository extends JpaRepository<Tags, Integer> {
+    Tags findTagsBy(String tag);
+
+    Tags findTagsById(int id);
 
 }
--- src/main/java/com/example/data/repository/WorkoutRepository.java
+++ src/main/java/com/example/data/repository/WorkoutRepository.java
@@ -1,5 +1,6 @@
 package com.example.data.repository;
 
+import com.example.data.model.User;
 import com.example.data.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,8 +9,7 @@ import java.util.UUID;
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 
-
-
+    Workout findWorkoutById(int id);
 
 }
 
