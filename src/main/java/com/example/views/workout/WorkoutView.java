455f486a6458320cf46938e1aeae124f35537eea
Ildar <khabibulin_ildarr@mail.ru>
2022-04-11 19:47:22 +0300

    added Workout page front

--- src/main/java/com/example/views/workout/WorkoutView.java
+++ src/main/java/com/example/views/workout/WorkoutView.java
@@ -1,63 +1,32 @@
 package com.example.views.workout;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.details.Details;
-import com.vaadin.flow.component.details.DetailsVariant;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.ListItem;
-import com.vaadin.flow.component.html.UnorderedList;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.example.data.service.WorkoutService;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-
-import javax.annotation.security.PermitAll;
 
 @PageTitle("Workout")
 @Route(value = "workout")
 //@PermitAll
 @AnonymousAllowed
-public class WorkoutView extends VerticalLayout {
-
-    private Component header;
-    private Label text;
+public class WorkoutView extends Div implements HasUrlParameter<Integer> {
+    private WorkoutService workoutService;
 
 
-    public WorkoutView() {
-        header = new H2("name_of_workout");
-        text = new Label("do hard");
-        text.setWidth("70%");
-
-        RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
-        radioGroup.setLabel("Please rate the workout");
-        radioGroup.setItems("1", "2","3","4","5");
-        HorizontalLayout rateButtons = new HorizontalLayout(radioGroup);
-        radioGroup.addValueChangeListener(e -> System.out.println("ну надо что-то считать"));
-
-        UnorderedList content = new UnorderedList(
-                new ListItem("бицепс"),
-                new ListItem("глаз")
-        );
-
-        content.add();
-
-        Details details = new Details("Tags", content);
-        details.setOpened(true);
-        details.addThemeVariants(DetailsVariant.REVERSE);
-        details.setWidth("70%");
-
-        setSizeFull();
-
-        setHorizontalComponentAlignment(Alignment.END);
-        setHorizontalComponentAlignment(Alignment.CENTER, header);
-        setHorizontalComponentAlignment(Alignment.CENTER, text);
-        setHorizontalComponentAlignment(Alignment.CENTER,rateButtons);
-        setHorizontalComponentAlignment(Alignment.CENTER,details);
-
-        add(header,details, text, rateButtons);
+    public WorkoutView(WorkoutService workoutService) {
+        this.workoutService=workoutService;
     }
 
+    @Override
+    public void setParameter(BeforeEvent event, Integer parameter) {
+        addWorkoutForm(workoutService,parameter);
+    }
+
+    private void addWorkoutForm(WorkoutService workoutService, int parameter){
+        WorkoutForm workoutForm = new WorkoutForm(workoutService, parameter);
+        add(workoutForm);
+    }
 }
