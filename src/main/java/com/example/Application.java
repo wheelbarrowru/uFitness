098e3d7b39eb59bd451183747d1753a58f9fedce
mvtValeev <75842546+mvtValeev@users.noreply.github.com>
2022-04-16 18:22:44 +0300

    swagger fixed

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The entry point of the Spring Boot application.
@@ -19,6 +20,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @Theme(value = "ufitness", variant = Lumo.DARK)
 @PWA(name = "uFitness", shortName = "uFitness", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+@EnableSwagger2
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
