c3e8c66b187f9fbd4fb9693dcc8d11ee7b216aea
mvtValeev <75842546+mvtValeev@users.noreply.github.com>
2022-05-08 01:50:15 +0300

    added methods in UserServiceTest

--- src/test/java/ru/mipt/data/service/UserServiceTest.java
+++ src/test/java/ru/mipt/data/service/UserServiceTest.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
 import ru.mipt.data.Role;
 import ru.mipt.data.dto.UserDTO;
+import ru.mipt.data.model.Tags;
 import ru.mipt.data.model.User;
+import ru.mipt.data.model.Workout;
 import ru.mipt.data.repository.UserRepository;
 
 import java.util.*;
@@ -21,7 +23,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static ru.mipt.data.Role.USER;
 
 class UserServiceTest {
-    public ArrayList<User> arraylist = new ArrayList<User>();
+    public ArrayList<User> arraylist = new ArrayList<>();
+    public ArrayList<User> arraylist2 = new ArrayList<>();
+    public ArrayList<User> arraylist3 = new ArrayList<>();
+    public ArrayList<User> arraylist4 = new ArrayList<>();
     @InjectMocks
     private UserRepository userRepository =new UserRepository(){
 
@@ -102,6 +107,17 @@ class UserServiceTest {
             Set<Role> roles = new HashSet<>();
             roles.add(USER);
             user.setRoles(roles);
+            Workout workout = new Workout();
+            workout.setId(0);
+            workout.setRating(5);
+            workout.setDescription("test");
+            workout.setTitle("test");
+            workout.setAuthorId(0);
+            HashSet<Tags> hashSet = new HashSet<>();
+            workout.setWorkoutTags(hashSet);
+            Set<Workout> workouts = new HashSet<>();
+            workouts.add(workout);
+            user.setFavoriteWorkouts(workouts);
             return integer==0? Optional.of(user) : Optional.empty();
         }
 
@@ -198,6 +214,17 @@ class UserServiceTest {
             Set<Role> roles = new HashSet<>();
             roles.add(USER);
             user.setRoles(roles);
+            Workout workout = new Workout();
+            workout.setId(0);
+            workout.setRating(5);
+            workout.setDescription("test");
+            workout.setTitle("test");
+            workout.setAuthorId(0);
+            HashSet<Tags> hashSet = new HashSet<>();
+            workout.setWorkoutTags(hashSet);
+            Set<Workout> workouts = new HashSet<>();
+            workouts.add(workout);
+            user.setFavoriteWorkouts(workouts);
             array.add(user);
 
             if (username.equals(array.get(0).getUsername())) {
@@ -220,6 +247,17 @@ class UserServiceTest {
             Set<Role> roles = new HashSet<>();
             roles.add(USER);
             user.setRoles(roles);
+            Workout workout = new Workout();
+            workout.setId(0);
+            workout.setRating(5);
+            workout.setDescription("test");
+            workout.setTitle("test");
+            workout.setAuthorId(0);
+            HashSet<Tags> hashSet = new HashSet<>();
+            workout.setWorkoutTags(hashSet);
+            Set<Workout> workouts = new HashSet<>();
+            workouts.add(workout);
+            user.setFavoriteWorkouts(workouts);
             return user;
         }
 
@@ -236,6 +274,17 @@ class UserServiceTest {
             Set<Role> roles = new HashSet<>();
             roles.add(USER);
             user.setRoles(roles);
+            Workout workout = new Workout();
+            workout.setId(0);
+            workout.setRating(5);
+            workout.setDescription("test");
+            workout.setTitle("test");
+            workout.setAuthorId(0);
+            HashSet<Tags> hashSet = new HashSet<>();
+            workout.setWorkoutTags(hashSet);
+            Set<Workout> workouts = new HashSet<>();
+            workouts.add(workout);
+            user.setFavoriteWorkouts(workouts);
             array.add(user);
 
             if (email.equals(array.get(0).getEmail())) {
@@ -248,17 +297,32 @@ class UserServiceTest {
 
         @Override
         public void addFavoriteWorkouts(int userId, int workoutId) {
-
+            Set<Workout> set = arraylist4.get(userId).getFavoriteWorkouts();
+            Workout workout1 = new Workout();
+            workout1.setId(workoutId);
+            workout1.setRating(5);
+            workout1.setDescription("test1");
+            workout1.setTitle("test1");
+            workout1.setAuthorId(0);
+            HashSet<Tags> hashSet = new HashSet<>();
+            workout1.setWorkoutTags(hashSet);
+            set.add(workout1);
+            arraylist4.get(0).setFavoriteWorkouts(set);
         }
 
         @Override
         public void removeFavoriteWorkouts(int userId, int workoutId) {
-
+            Set<Workout> set = arraylist3.get(0).getFavoriteWorkouts();
+            set.removeIf(x -> x.getId() == workoutId);
+            arraylist3.get(0).setFavoriteWorkouts(set);
         }
 
         @Override
         public void updateUserInfo(int userId, String username, String firstname, String lastName, String email) {
-
+            arraylist2.get(0).setFirstname("test1");
+            arraylist2.get(0).setEmail("test1");
+            arraylist2.get(0).setLastname("test1");
+            arraylist2.get(0).setUsername("username1");
         }
 
     };
@@ -279,6 +343,17 @@ class UserServiceTest {
         Set<Role> roles = new HashSet<>();
         roles.add(USER);
         user.setRoles(roles);
+        Workout workout = new Workout();
+        workout.setId(0);
+        workout.setRating(5);
+        workout.setDescription("test");
+        workout.setTitle("test");
+        workout.setAuthorId(0);
+        HashSet<Tags> hashSet = new HashSet<>();
+        workout.setWorkoutTags(hashSet);
+        Set<Workout> workouts = new HashSet<>();
+        workouts.add(workout);
+        user.setFavoriteWorkouts(workouts);
         Assertions.assertEquals(Optional.of(user), userService.get(0));
     }
 
@@ -296,7 +371,18 @@ class UserServiceTest {
         Set<Role> roles = new HashSet<>();
         roles.add(USER);
         user1.setRoles(roles);
-        Assertions.assertEquals(userService.convertToUser(userDTO), user1);
+        Workout workout = new Workout();
+        workout.setId(0);
+        workout.setRating(5);
+        workout.setDescription("test");
+        workout.setTitle("test");
+        workout.setAuthorId(0);
+        HashSet<Tags> hashSet = new HashSet<>();
+        workout.setWorkoutTags(hashSet);
+        Set<Workout> workouts = new HashSet<>();
+        workouts.add(workout);
+        user1.setFavoriteWorkouts(workouts);
+        Assertions.assertEquals(UserService.convertToUser(userDTO), user1);
     }
 
     @Test
@@ -313,12 +399,13 @@ class UserServiceTest {
         Set<Role> roles = new HashSet<>();
         roles.add(USER);
         user1.setRoles(roles);
+
         Assertions.assertEquals(user1, userService.update(userDTO));
     }
 
     @Test
     void delete() {
-        userRepository.deleteById(0);
+        userService.delete(0);
         ArrayList<User> ArrayList = new ArrayList<User>();
         Assertions.assertEquals(ArrayList, arraylist);
     }
@@ -384,5 +471,112 @@ class UserServiceTest {
         UserDTO userDTO1 = new UserDTO();
         Assertions.assertEquals(UserService.convertToUserDTO(userService.get(0).orElse(new User())), userDTO);
         Assertions.assertEquals(UserService.convertToUserDTO(userService.get(222).orElse(new User())), userDTO1);
+    }
+    @Test
+    void checkNotExistUsernameWithDifferentID(){
+
+    }
+    @Test
+    void checkNotExistEmailWithDifferentID(){
+
+    }
+    @Test
+    void getFavoriteWorkouts(){
+
+    }
+    @Test
+    void addFavoriteWorkout(){
+        User user1 = new User();
+        user1.setEmail("test@mail.ru");
+        user1.setUsername("username");
+        user1.setFirstname("test");
+        user1.setLastname("test");
+        user1.setPassword("test");
+        user1.setId(0);
+        Set<Role> roles = new HashSet<>();
+        roles.add(USER);
+        user1.setRoles(roles);
+        Workout workout1 = new Workout();
+        workout1.setId(0);
+        workout1.setRating(5);
+        workout1.setDescription("test1");
+        workout1.setTitle("test1");
+        workout1.setAuthorId(0);
+        HashSet<Tags> hashSet = new HashSet<>();
+        workout1.setWorkoutTags(hashSet);
+        arraylist4.add(user1);
+        userService.addFavoriteWorkout(0,0);
+        Set<Workout> workouts1 = new HashSet<>();
+        workouts1.add(workout1);
+        assertEquals(arraylist4.get(0).getFavoriteWorkouts(), workouts1);
+    }
+    @Test
+    void removeFavoriteWorkouts(){
+        User user1 = new User();
+        user1.setEmail("test@mail.ru");
+        user1.setUsername("username");
+        user1.setFirstname("test");
+        user1.setLastname("test");
+        user1.setPassword("test");
+        user1.setId(0);
+        Set<Role> roles = new HashSet<>();
+        roles.add(USER);
+        user1.setRoles(roles);
+        Workout workout = new Workout();
+        workout.setId(0);
+        workout.setRating(5);
+        workout.setDescription("test");
+        workout.setTitle("test");
+        workout.setAuthorId(0);
+        Workout workout1 = new Workout();
+        workout1.setId(1);
+        workout1.setRating(5);
+        workout1.setDescription("test1");
+        workout1.setTitle("test1");
+        workout1.setAuthorId(0);
+        HashSet<Tags> hashSet = new HashSet<>();
+        workout.setWorkoutTags(hashSet);
+        workout1.setWorkoutTags(hashSet);
+        Set<Workout> workouts = new HashSet<>();
+        workouts.add(workout);
+        workouts.add(workout1);
+        Set<Workout> workouts1 = new HashSet<>();
+        workouts1.add(workout1);
+        user1.setFavoriteWorkouts(workouts);
+        arraylist3.add(user1);
+        userService.removeFavoriteWorkouts(0,0);
+        Assertions.assertEquals(arraylist3.get(0).getFavoriteWorkouts(), workouts1);
+    }
+    @Test
+    void checkFavorite(){
+        assertTrue(userService.checkFavorite(0, 0));
+        assertFalse(userService.checkFavorite(222, 0));
+
+    }
+    @Test
+    void updateUserInfo(){
+        User user1 = new User();
+        user1.setEmail("test@mail.ru");
+        user1.setUsername("username");
+        user1.setFirstname("test");
+        user1.setLastname("test");
+        user1.setPassword("test");
+        user1.setId(0);
+        Set<Role> roles = new HashSet<>();
+        roles.add(USER);
+        user1.setRoles(roles);
+        arraylist2.add(user1);
+        User user = new User();
+        user.setEmail("test@mail.ru");
+        user.setUsername("username1");
+        user.setFirstname("test1");
+        user.setLastname("test1");
+        user.setPassword("test1");
+        user.setId(0);
+        Set<Role> roles1 = new HashSet<>();
+        roles.add(USER);
+        user1.setRoles(roles1);
+        userService.updateUserInfo(0,"test1", "test1", "test", "test");
+        Assertions.assertEquals(arraylist2.get(0), user);
     }
 }
